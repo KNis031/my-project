cda11796950c5f9f8f1b8c8259b27fd20fcdb1fe
dferreras439 <36524216+dferreras439@users.noreply.github.com>
2020-05-03 20:12:17 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -5,7 +5,15 @@ var url = 'http://newsapi.org/v2/top-headlines?' +
 var req = new Request(url);
 fetch(req)
     .then(function(response) {
-      
-      var data = JSON.parse(this.response) // kanske ska va (response) ist för this.response
+          var data = JSON.parse(this.response.json() ) // kanske ska va (response) ist för this.response
+        
+          var title1 = data.articles[0].title;
+          var title2 = data.articles[1].title;
+          var title3 = data.articles[2].title;
+          
+          document.getElementById("headline1").innerHTML = title1;
+          document.getElementById("headline2").innerHTML = title2;
+          document.getElementById("headline3").innerHTML = title3;
+          
       
     })
