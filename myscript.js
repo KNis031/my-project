d9970ec4b55d0cb677684c8ca1faddba26e4cc31
dferreras439 <36524216+dferreras439@users.noreply.github.com>
2020-05-03 19:45:34 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,11 @@
 
+var url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=se&' +
+          'apiKey=5cd1a5286d9e453abceee179a6446b54';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+      
+      var data = JSON.parse(this.response) // kanske ska va (response) ist för this.response
+      
+    })
